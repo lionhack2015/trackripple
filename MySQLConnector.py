b6e5cd6aeece3521a9ee52abfacccefd71ef259a
Vishal Juneja <vishaljuneja@Vishals-MacBook-Pro.local>
2015-11-16 05:08:02 -0500

    MYSQL connectivity

---
+++ MySQLConnector.py
@@ -0,0 +1,9 @@
+# install MySQLdb to use this file http://stackoverflow.com/questions/1448429/how-to-install-mysqldb-python-data-access-library-to-mysql-on-mac-os-x#1448476
+# this is API for persisting data to MYSQL db
+
+# upload SQL file to github to enable recreation of local database instances
+# think about db migrations?
+
+
+import MYSQLdb
+
