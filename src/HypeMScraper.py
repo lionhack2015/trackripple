0d99377995b5665586210691026d3f6a0625fa49
Vishal Juneja <vishaljuneja@dyn-209-2-222-162.dyn.columbia.edu>
2015-11-19 15:42:04 -0500

    Blog stats + scraper fixes

--- src/HypeMScraper.py
+++ src/HypeMScraper.py
@@ -2,6 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 DATA_DIR = "../data/"
+
+STATS_NUM_BLOGS = 0
+STATS_NUM_BLOGS_WITHOUT_SC = 0
+STATS_NUM_BLOGS_WITHOUT_GENRES = 0
+STATS_NUM_BLOGS_WITHOUT_FOLLOWERS = 0
+STATS_AVG_SONG_COUNT = 0
+STATS_AVG_GENRE_COUNT = 0
     
 def scrape_country_url(url):
  
@@ -57,6 +64,10 @@ def scrape_countries():
 
 
 def scrape_each_blog(url):
+    global STATS_NUM_BLOGS_WITHOUT_SC
+    global STATS_NUM_BLOGS_WITHOUT_GENRES
+    global STATS_NUM_BLOGS_WITHOUT_FOLLOWERS
+
     driver = webdriver.Firefox()
     driver.get(url)
 
@@ -64,23 +75,34 @@ def scrape_each_blog(url):
     blog_link = driver.find_elements_by_class_name('visit-blog')[0].find_element_by_xpath('.//a').get_attribute('href')
 
     # get blog genres
-    genres_ul = driver.find_element_by_css_selector(".tags.small")
     genres_list = []
-    for tl in genres_ul.find_elements_by_tag_name('li'):
-       genres_list.append(tl.text)
+    try:
+        genres_ul = driver.find_element_by_css_selector(".tags.small")
+        for tl in genres_ul.find_elements_by_tag_name('li'):
+            genres_list.append(tl.text)
+    except:
+        STATS_NUM_BLOGS_WITHOUT_GENRES = STATS_NUM_BLOGS_WITHOUT_GENRES + 1
 
-    big_nums =  driver.find_elements_by_xpath("//span[@class='big-num']")
-    # tracks
-    track_num = big_nums[0].text
+    track_num = -1
+    followers_num = -1
+    try:
+        big_nums =  driver.find_elements_by_xpath("//span[@class='big-num']")
+        # tracks
+        track_num = big_nums[0].text
 
-    # followers
-    followers_num = big_nums[1].text
+        # followers
+        followers_num = big_nums[1].text
+    except:
+        STATS_NUM_BLOGS_WITHOUT_FOLLOWERS = STATS_NUM_BLOGS_WITHOUT_FOLLOWERS + 1
 
     # recent soundcloud music links
     sc_links = []
     # TODO: resolve this to acutal sound cloud links
-    sc_links.append(driver.find_element_by_class_name("icon-sc").get_attribute("href"))
-    play = driver.find_element_by_id("playerPlay").click()
+    try:
+        sc_links.append(driver.find_element_by_class_name("icon-sc").get_attribute("href"))
+        play = driver.find_element_by_id("playerPlay").click()
+    except:
+        STATS_NUM_BLOGS_WITHOUT_SC = STATS_NUM_BLOGS_WITHOUT_SC + 1
 
     sng_cnt = 0
     while sng_cnt < 100:
@@ -99,7 +121,17 @@ def scrape_each_blog(url):
 #scrape_each_blog("http://hypem.com/blog/abduction+radiation/21500")
 
 def persist_in_text_files():
+
+    global STATS_NUM_BLOGS
+    global STATS_NUM_BLOGS_WITHOUT_SC
+    global STATS_NUM_BLOGS_WITHOUT_FOLLOWERS
+    global STATS_NUM_BLOGS_WITHOUT_GENRES
+    global STATS_AVG_SONG_COUNT
+    global STATS_AVG_GENRE_COUNT
+
+    
     country_wise_urls = scrape_countries()
+    num_countries = len(country_wise_urls)
 
     f = open(DATA_DIR+"countrywise_blog_links.txt", "w")
     f_list = open(DATA_DIR+"countrywise_blog_list.txt", "w")
@@ -113,14 +145,27 @@ def persist_in_text_files():
 
         # scrape each blog_link for blog characterisitcs
         for bl in blog_list:
-            # persist
-            f.write(country+" "+bl+"\n")
+            STATS_NUM_BLOGS = STATS_NUM_BLOGS + 1
+            # process blog
             blg_id = bl.split("/")[-1]
             (blog_link, genres_list, track_num, followers_num, sc_links) = scrape_each_blog(bl)
+            # collect stats
+            STATS_AVG_SONG_COUNT += len(sc_links)
+            STATS_AVG_GENRE_COUNT += len(genres_list)
             # persist
+            f.write(country+" "+bl+"\n")
             f_blg.write(blg_id+" "+'|'.join(genres_list)+
-                    " "+track_num+" "+followers_num+" "+'|'.join(sc_links))
+                    " "+track_num+" "+followers_num+" "+'|'.join(sc_links)+"\n")
 
+
+    print  "FINISHED SCRAPING ... PRINTING STATS"
+    print  "STATS_NUM_BLOGS " + STATS_NUM_BLOGS
+    print  "STATS_BLOGS_PER_COUNTRY " + (STATS_NUM_BLOGS/len(num_countries))
+    print  "STATS_NUM_BLOGS_WITHOUT_SC " + STATS_NUM_BLOGS_WITHOUT_SC
+    print  "STATS_NUM_BLOGS_WITHOUT_FOLLOWERS " + STATS_NUM_BLOGS_WITHOUT_FOLLOWERS
+    print  "STATS_NUM_BLOGS_WITHOUT_GENRES " + STATS_NUM_BLOGS_WITHOUT_GENRES
+    print  "STATS_AVG_SONG_COUNT_PER_BLOG "+ (STATS_AVG_SONG_COUNT/STATS_NUM_BLOGS)
+    print  "STATS_AVG_GENRE_COUNT_PER_BLOG "+ (STATS_AVG_GENRE_COUNT/STATS_NUM_BLOGS)
 
     f.close()
     f_list.close()
